b2a8ee76aa5446567942136cce897fc2355d1c75
Sandino204 <caiogustavoxaviermelo@gmail.com>
2020-08-14 19:04:09 -0300

    Project Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import youtube from './api/youtube'
+import SearchBar from './components/searchBar'
+import VideoDetail from './components/videoDetail'
+import VideoList from './components/videoItem'
 
 function App() {
+
+  const [videos, setVideos] = useState([])
+  const [selectedVideo, setSelectedVideo] = useState(null)
+
+  async function onFormSubmit(search){
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet', 
+        maxResults: 5, 
+        key: '', 
+        q: search
+      }
+    });
+    
+    setVideos(response.data.items)
+    setSelectedVideo(response.data.items[0])
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid page">
+        <SearchBar func={onFormSubmit}></SearchBar>
+        <div className="row mt-5 ml-4">
+          <div className="col-12 col-md-8 row d-flex justify-content-center">
+              <VideoDetail video={selectedVideo}></VideoDetail>
+          </div>
+          <div className="col-4 row">
+              <VideoList videos={videos}></VideoList>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
